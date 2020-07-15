c75ca3e33fe0f65c6cf4bfcf28042ceca4743a97
Ricardo <rbalamcm@gmail.com>
2020-07-15 15:20:45 -0500

    Primer commit

---
+++ js/sw_utils.js
@@ -0,0 +1,11 @@
+// Guarda el cache dinamico
+function actualizaCacheDinamico(dynamicCache,req,res){
+       if(res.ok){
+           return caches.open(dynamicCache).then (cache =>{
+               cache.put(req, res.clone());
+               return res.clone();
+           });
+       } else {
+           return res;
+       }
+}
